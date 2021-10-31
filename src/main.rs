b82d6dd8c4de47c61626a833bb8efec75a9289d2
carlosziegler <carlos.ziegler@gmail.com>
2021-10-31 11:00:21 +0100

    create game

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
+extern crate  rand;
 use std::io; // import statement
+use std::cmp::Ordering; // import statement
+use rand::Rng;
 
 fn main() {
     println!("Advinhe um numero");
-    println!("Digite seu palpite");
-
-    let mut palpite = String::new();
-
-    io::stdin().read_line(&mut palpite)
-        .expect("Falha ao ler entrada");
-
-    println!("Voce disse {}", palpite);
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    
+    loop{        
+        println!("Digite seu palpite");
+    
+        let mut input = String::new();
+    
+        io::stdin().read_line(&mut input)
+            .expect("Falha ao ler entrada");
+        
+        let input: u32 = match input.trim().parse() {
+            Ok(num)=> num,
+            Err(_) => {
+                println!("{} nao e um numero!", input.trim());
+                continue
+            },
+        };
+    
+        println!("Voce disse {}", input);
+    
+        match input.cmp(&secret_number){
+            Ordering::Less => println!("Muito baixo!"),
+            Ordering::Greater => println!("Muito alto!"),
+            Ordering::Equal => {
+                println!("Voce acertou!");
+                break;
+            }
+        }
+    }
 }
